b3b9c5701c8d6dd3dee5d36b44b65d0e9c0a7ad1
Nantamil <sanmkr17@gmail.com>
2026-09-20 23:23:47 +0530

    remove fb, yt

--- script.js
+++ script.js
@@ -85,9 +85,7 @@ function buildFooter(){
       <h3 style="margin-top:28px;">Follow Us</h3>
       <div class="social-icons">
         <a href="https://www.instagram.com/rotaractdistrict3206/" aria-label="Instagram">IG</a>
-        <a href="#" aria-label="Facebook" data-placeholder-link>FB</a>
         <a href="https://www.linkedin.com/company/rotaract-club-of-coimbatore-institute-of-technology/" aria-label="LinkedIn">IN</a>
-        <a href="#" aria-label="YouTube" data-placeholder-link>YT</a>
       </div>
     </div>
     <div class="address-block">
